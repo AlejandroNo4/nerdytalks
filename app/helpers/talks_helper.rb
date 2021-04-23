269945018f4ed8e453fd4a503c7dad90be3b3432
Alejandro Contreras <kdta15@outlook.com>
2021-04-22 19:05:09 -0500

    Add style to the form for create a new talk. Rearrange the home layout containers.

--- app/helpers/talks_helper.rb
+++ app/helpers/talks_helper.rb
@@ -3,4 +3,10 @@ module TalksHelper
     @user = User.find_by(id: talkid)
     @user.username
   end
+
+  def destroy_talk(talk)
+    if logged_in? && current_user.username == author_username(talk.author_id)
+      link_to 'Destroy', talk, method: :delete, data: { confirm: 'Are you sure?' }
+    end
+  end
 end
